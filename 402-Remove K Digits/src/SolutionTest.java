6a40387fffb38778215f1e7362090b4a8b385f61
张庚昕 <wuminorb@gmail.com>
2017-07-13 12:31:01 +0800

    402-Remove K Digits

--- 402-Remove K Digits/src/SolutionTest.java
+++ 402-Remove K Digits/src/SolutionTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 public class SolutionTest {
     @Test
     public void test() {
-        System.out.println(new String("5 D; a1 e  l' i7 T"));
         assertEquals(new Solution().removeKdigits("5337", 2), "33");
         assertEquals(new Solution().removeKdigits("1432219", 3), "1219");
         assertEquals(new Solution().removeKdigits("10200", 1), "200");
